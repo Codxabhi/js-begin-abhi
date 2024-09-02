5224f4a34cc8b60f2f44f8d5c47ba8ebf3d357d9
Codxabhi <166607528+Codxabhi@users.noreply.github.com>
2024-09-02 19:07:51 +0000

    datatype done

--- .devcontainer/01_basics/start.js
+++ .devcontainer/01_basics/start.js
@@ -2,3 +2,27 @@ const name="abhi"
 const city="dhampur"
 console.log(name)
 console.log(city)
+let num
+let age=431
+const class1= "seven"
+console.log(class1)
+console.log(age)
+//types of data types used boolean,null,undefined,string,integer
+//  (typeof ) is the number is used to find the type of it 
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null)
+let score=44
+console.log(typeof score)
+let Number="46"
+console.log(Number)
+console.log(typeof Number)
+let Number1="34abde"
+console.log(Number1)
+console.log(typeof Number1)
+
+
+
+
+
+ 
