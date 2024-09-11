b6ce50d916d2c7c56d2a1c1af97ee6aa5c45e5d3
Codxabhi <166607528+Codxabhi@users.noreply.github.com>
2024-09-11 19:41:20 +0000

    datatye

---
+++ .devcontainer/01_basics/datatype.js
@@ -0,0 +1,8 @@
+let firstvalue="helloname"
+let secondvalue="firstvalue"
+secondvalue="myname"
+console.log(firstvalue)
+console.log(secondvalue)
+//when we put in stack then we get only the copy of it
+//but when we put in the heap we only get the reference of it
+
