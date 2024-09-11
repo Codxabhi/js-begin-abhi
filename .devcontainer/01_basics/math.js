4cf6be3a2e98e363aa9365ba414f329db3f60051
Codxabhi <166607528+Codxabhi@users.noreply.github.com>
2024-09-11 19:44:07 +0000

    maths complete

---
+++ .devcontainer/01_basics/math.js
@@ -0,0 +1,7 @@
+//maths
+
+console.log(Math.abs(-3))//change the negative value
+console.log(Math.round(66.7))
+console.log(Math.random())
+//Math.random gives random value but it guves the value in between 0 & 1
+
