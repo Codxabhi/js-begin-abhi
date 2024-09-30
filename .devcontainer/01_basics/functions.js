387a726c01bacb3c88c8dd2d0f57d86886957804
Codxabhi <166607528+Codxabhi@users.noreply.github.com>
2024-09-30 17:49:10 +0000

    functions done

---
+++ .devcontainer/01_basics/functions.js
@@ -0,0 +1,73 @@
+//function
+console.log("A")
+console.log("B")
+console.log("h")
+
+
+function saymyname(){//this is how we can decalare ~ (funcation name(){in this we will declare} ) 
+
+    console.log("abhu")
+    console.log("asdv")
+   console.log("adAF")
+   console.log("NFSKNB")
+   console.log("JIKSS")
+
+}
+saymyname()//this is the execuation of the function
+//paramters
+//arguemnets
+function addingstring (string1,string2){
+    console.log(string1 + string2)
+    return string1 + string2
+
+}
+addingstring("ab","vc")
+function loginuser(username){
+    return `${username} just logged in`
+
+}
+console.log(loginuser("hello"))
+
+function user(value,value1)
+{
+    if(user === undefined){
+        console.log("hello my name")
+        return 
+    }
+    return value + value1
+
+}
+console.log(user(6,4))
+//starting with chatgpt
+
+function start()//this shows how a function is defiend
+{
+   //code is returned
+}
+//ex1
+
+function add(value1,value2)
+{
+    return value1 + value2
+
+
+}
+console.log(add(6,1))
+
+function tocheck(a,b)
+{
+    
+}
+
+function calcprice(num1)
+{
+    return num1
+
+}
+console.log(calcprice(200))
+console.log(calcprice(300,500,400))
+function calci(...num2){//here dot dot implies rest operator
+    return num2
+}
+console.log(calci(200,400,1000))
+
